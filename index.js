940e4eadc104d8b174d6e15bcbdd5766368ab3c8
Naod Hunde <chris_naod@yahoo.com>
2023-02-16 15:30:45 +1100

    function to start the app with promptes

--- index.js
+++ index.js
@@ -9,4 +9,76 @@ const connection = mysql.createConnection({
     password: 'password',
     database: 'employee_tracker'
   });
-  
+
+  // Start the application
+function start() {
+    inquirer.prompt([
+      {
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+          'Update an employee manager',
+          'View employees by manager',
+          'View employees by department',
+          'Delete a department',
+          'Delete a role',
+          'Delete an employee',
+          'Exit'
+        ]
+      }
+    ])
+    .then((answer) => {
+      switch (answer.action) {
+        case 'View all departments':
+          viewAllDepartments();
+          break;
+        case 'View all roles':
+          viewAllRoles();
+          break;
+        case 'View all employees':
+          viewAllEmployees();
+          break;
+        case 'Add a department':
+          addDepartment();
+          break;
+        case 'Add a role':
+          addRole();
+          break;
+        case 'Add an employee':
+          addEmployee();
+          break;
+        case 'Update an employee role':
+          updateEmployeeRole();
+          break;
+        case 'Update an employee manager':
+          updateEmployeeManager();
+          break;
+        case 'View employees by manager':
+          viewEmployeesByManager();
+          break;
+        case 'View employees by department':
+          viewEmployeesByDepartment();
+          break;
+        case 'Delete a department':
+          deleteDepartment();
+          break;
+        case 'Delete a role':
+          deleteRole();
+          break;
+        case 'Delete an employee':
+          deleteEmployee();
+          break;
+        case 'Exit':
+          connection.end();
+          break;
+      }
+    });
+  }
