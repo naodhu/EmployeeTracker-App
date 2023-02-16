bc26231846bdd5b2e8e34a1ebac1e1202bf2271a
Naod Hunde <chris_naod@yahoo.com>
2023-02-16 15:26:13 +1100

    created connection to the database

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = requrie("console.table");
+
+// create a connection to the database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password', // Replace with your own password
+    database: 'employee_tracker'
+  });
+  
+  // connect to the database
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log(`Connected as id ${connection.threadId}`);
+    start();
+  });
